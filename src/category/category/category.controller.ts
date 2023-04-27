7e61efa10eb1ebbfdc87d62d06e9c240d043220d
Warczachowski <wlodek_w@yahoo.de>
2023-04-27 18:45:16 +0200

    still work on categories

--- src/category/category/category.controller.ts
+++ src/category/category/category.controller.ts
@@ -1,12 +1,12 @@
 import {
-  Body,
-  Controller,
-  Delete,
-  Get,
-  Param,
-  Post,
-  Put,
-  UseGuards,
+    Body,
+    Controller,
+    Delete,
+    Get,
+    Param,
+    Post,
+    Put,
+    UseGuards,
 } from '@nestjs/common';
 import { ArtikelCategory } from 'src/entity/artikelKategoryEntity';
 import { CategoryService } from './category.service';
@@ -16,31 +16,31 @@ import { CategoryDTO } from 'src/dto/categoryDTO';
 
 @Controller('category')
 export class CategoryController {
-  constructor(private readonly categoryService: CategoryService) {}
+    constructor(private readonly categoryService: CategoryService) {}
 
-  @Get()
-  async findAll(): Promise<ArtikelCategory[]> {
-    return await this.categoryService.findAll();
-  }
+    @Get()
+    async findAll(): Promise<ArtikelCategory[]> {
+        return await this.categoryService.findAll();
+    }
 
-  @Post()
-  @UseGuards(AuthGuard('jwt'), RoleGuard)
-  async create(@Body() category: CategoryDTO): Promise<ArtikelCategory> {
-    return await this.categoryService.create(category);
-  }
+    @Post()
+    @UseGuards(AuthGuard('jwt'), RoleGuard)
+    async create(@Body() category: CategoryDTO): Promise<ArtikelCategory> {
+        return await this.categoryService.create(category);
+    }
 
-  @Put(':id')
-  @UseGuards(AuthGuard('jwt'), RoleGuard)
-  async update(
-    @Param('id') id: number,
-    @Body() category: ArtikelCategory,
-  ): Promise<number> {
-    return await this.categoryService.update(id, category);
-  }
+    @Put(':id')
+    @UseGuards(AuthGuard('jwt'), RoleGuard)
+    async update(
+        @Param('id') id: number,
+        @Body() category: ArtikelCategory,
+    ): Promise<number> {
+        return await this.categoryService.update(id, category);
+    }
 
-  @Delete(':id')
-  @UseGuards(AuthGuard('jwt'), RoleGuard)
-  async delete(@Param('id') id: number): Promise<number> {
-    return await this.categoryService.delete(id);
-  }
+    @Delete(':id')
+    @UseGuards(AuthGuard('jwt'), RoleGuard)
+    async delete(@Param('id') id: number): Promise<number> {
+        return await this.categoryService.delete(id);
+    }
 }
